4a9820218a40c2642a1a35d0a6ef9bb9f82ea58d
Edwardscg <edwardcornegomez@mail.com>
2024-07-21 20:15:23 -0500

    Actualización; 2 ejercicios más.

---
+++ C#Project_005/Reverse-Chain/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Ingresa una frase: ");
+string? chain = Console.ReadLine();
+if (chain != null)
+{
+    string reverseText = InvertirFrase(chain);
+    Console.WriteLine("Frase invertida");
+    Console.WriteLine(reverseText);
+}
+else
+{
+    Console.WriteLine("No has ingresado una frase válida");
+}
+
+static string InvertirFrase(string text)
+{
+    char[] caracteres = text.ToCharArray();
+    Array.Reverse(caracteres);
+    return new string(caracteres);
+}
---
+++ C#Project_006/Guess-Number/Program.cs
@@ -0,0 +1,73 @@
+﻿Random rmd = new Random();
+
+Console.WriteLine("Bienvenido al juego");
+Console.WriteLine("1. Jugar a adivina el número");
+Console.WriteLine("0. Salir");
+int option = Convert.ToInt32(Console.ReadLine());
+
+do
+{
+    switch (option)
+    {
+        case 1:
+
+            int randomNum = rmd.Next(1, 11);
+            Console.WriteLine("Adivina un número entre 0 y 10:");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            do
+            {
+                if (number < randomNum)
+                {
+                    Console.WriteLine("Tu número es menor, prueba otro más alto.");
+                    number = Convert.ToInt32(Console.ReadLine());
+                }
+                else
+                {
+                    Console.WriteLine("Tu número es mayor, prueba otro más bajo.");
+                    number = Convert.ToInt32(Console.ReadLine());
+                }
+            } while (number != randomNum);
+            if (number == randomNum)
+            {
+                Console.WriteLine("Has ganado!");
+            }
+            Console.WriteLine("");
+            break;
+        case 0:
+            Console.WriteLine("Gracias por jugar!");
+            break;
+    }
+
+    Console.WriteLine("1. Jugar a adivina el número");
+    Console.WriteLine("0. Salir");
+    option = Convert.ToInt32(Console.ReadLine());
+
+    Console.WriteLine("Gracias por jugar!");
+} while (option != 0);
+
+//Version whitout switch
+
+// int randomNum = rmd.Next(0, 11);
+// Console.WriteLine("Adivina un número entre 0 y 10:");
+// int number = Convert.ToInt32(Console.ReadLine());
+
+// do
+// {
+//     if (number < randomNum)
+//     {
+//         Console.WriteLine("Tu número es menor, prueba otro más alto.");
+//         number = Convert.ToInt32(Console.ReadLine());
+//     }
+//     else
+//     {
+//         Console.WriteLine("Tu número es mayor, prueba otro más bajo.");
+//         number = Convert.ToInt32(Console.ReadLine());
+//     }
+
+// } while (number != randomNum);
+
+// if (number == randomNum)
+// {
+//     Console.WriteLine("Has ganado!");
+// }
